ef931ef09634c62264b7be071d848907a01291a4
sangdothanh95 <sangdothanh95@gmail.com>
2025-12-10 11:14:24 +0700

    add Identity Server4

---
+++ src/Identity/Identity.API/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Identity.API.Database.Configuration;
+using Identity.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.API.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+        }
+    }
+}
